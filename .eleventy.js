ff654352dc069e781634a8a2b0a2ea3048def086
qq7 <qq7dev@gmail.com>
2021-09-28 19:11:31 +0300

    src

--- .eleventy.js
+++ .eleventy.js
@@ -65,7 +65,7 @@ module.exports = function (eleventyConfig) {
   // So that we can use .html instead of .njk
   return {
     dir: {
-      input: `src/${locale}`,
+      input: "src",
     },
     htmlTemplateEngine: "njk",
   };
